79a67e600a226b0deb88d96a3a1209be88eb140b
Johnny Packard <packard81@me.com>
2018-06-26 09:58:48 -0500

    added a second reducer and imported combineReducers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
       <div>
         {/* Dispatching an action */}
         <button onClick={() => this.props.dispatch({ type: 'BUTTON_ONE' })}>Button One</button>
+        <button onClick={() => this.props.dispatch({ type: 'BUTTON_TWO' })}>Button Two</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+
+// This is our first reducer
+const firstReducer = (state, action) => {
+    console.log(`What up, yo? I'm a reducer!`);
+    return {};
+};
+
+const secondReducer = (state, action) => {
+    console.log(`Whoa, lookout, I'm another reducer!`);
+    return {};
+};
 
 // This is creating the store
 // The store is a big JS Object that holds
 // all of the information for our app
 const storeInstance = createStore(
-    // This is a reducer
-    () => {
-        console.log(`What up, yo, I'm a reducer`);
-    },
+    combineReducers({
+        firstReducer,
+        secondReducer
+    }),
 );
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
